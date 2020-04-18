681ef7182e8be273c8982678b3519e4804ea93de
SethPaxton <spaxton0492@gmail.com>
2020-04-18 06:29:56 -0700

    Base functionality added
    You can move the world (and the sky/moon/sun with it), there's a stub for the space station and the tractor beam, added model of the world & texture

---
+++ LD46_Alive/Assets/Code/scr_rotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_rotation : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 100f;
+    bool dragging = false;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDrag()
+    {
+        dragging = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (dragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+            float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+            rb.AddTorque(Vector3.down * x);
+            rb.AddTorque(Vector3.right * y);
+        }
+    }
+}
