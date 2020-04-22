f8e81bb496cb53db611304197db0c0f1abaaaee0
composerben <56130322+composerben@users.noreply.github.com>
2020-04-22 16:54:46 -0700

    ADAPTIVE MUSIC BITCHES!!!!!

--- LD46_Alive/Assets/Code/scr_HPeval.cs
+++ LD46_Alive/Assets/Code/scr_HPeval.cs
@@ -7,11 +7,14 @@ public class scr_HPeval : MonoBehaviour
     public bool at70 = false;
     public bool at30 = false;
     [SerializeField] GameObject player;
+	private static FMOD.Studio.EventInstance Music;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+		Music = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Music");
+		Music.start();
+		Music.release();
     }
 
     // Update is called once per frame
@@ -21,10 +24,16 @@ public class scr_HPeval : MonoBehaviour
         if (hp <= 70)
         {
             at70 = true;
+			Music.setParameterByName("Health", 1f);
         }
         if (hp <= 30)
         {
             at30 = true;
+			Music.setParameterByName("Health", 2f);
         }
     }
+    private void OnDestroy()
+	{
+		Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+	}
 }
